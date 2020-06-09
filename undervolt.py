82bf6fb252299469da3b9b4b4ff8d758c36ea65a
Berat Postalcioglu <berat.postalcioglu@ekinokssoftware.com>
2020-06-09 16:21:35 +0300

    use assert_root to check whether user is root when '-r' option is used

--- undervolt.py
+++ undervolt.py
@@ -393,6 +393,7 @@ def main():
     msr = ADDRESSES
 
     if not glob('/dev/cpu/*/msr'):
+        assert_root()
         subprocess.check_call(['modprobe', 'msr'])
 
     if (args.core or args.cache) and args.core != args.cache:
